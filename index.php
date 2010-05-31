5ca4215b6ad3b601d019c5e256f4fb93e1493556
Jan Luca <jan@jans-seite.de>
2010-05-31 09:34:18 +0000

    Add version number to pre-beta

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ header('Content-type: text/html; charset=utf-8');
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html> 
 <head> 
-<title>CommonsHelper 2</title> 
+<title>CommonsHelper 2 <small>v1 pre-beta</small></title> 
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head>
 <body>
